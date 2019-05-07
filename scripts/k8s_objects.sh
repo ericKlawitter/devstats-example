cffb0fab72fa2ae2017267a51ce61cd908947680
Eric Klawitter <5823687+ericKlawitter@users.noreply.github.com>
2019-05-07 16:44:27 -0700

    Rename files, pull repo on cron, use default namespace

--- scripts/k8s_objects.sh
+++ scripts/k8s_objects.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-kubectl create -f Kubernetes/namespace_pg.yaml
-kubectl config set-context $(kubectl config current-context) --namespace=devstats-postgres
-kubectl create -f Kubernetes/env_variables.yaml
-kubectl create -f Kubernetes/nfs_pvc.yaml
-kubectl create -f Kubernetes/pg_service.yaml
-kubectl create -f Kubernetes/cli-statefulset.yaml
+kubectl create -f Kubernetes/env-variables.yaml
+kubectl create -f Kubernetes/volumes.yaml
+kubectl create -f Kubernetes/pg-service.yaml
+kubectl create -f Kubernetes/cli-home.yaml
