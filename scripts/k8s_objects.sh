a7d8d0abb25d21d49edd422a4a0c56f7970c6bf4
Eric Klawitter <5823687+ericKlawitter@users.noreply.github.com>
2019-05-08 17:22:38 -0700

    update datasource, use secrets. Need to verify whether we need ro_user

--- scripts/k8s_objects.sh
+++ scripts/k8s_objects.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+func random_string() {
+  `< /dev/urandom tr -dc A-Za-z0-9 | head -c14; echo`
+}
 kubectl create -f Kubernetes/env-variables.yaml
 kubectl create -f Kubernetes/volumes.yaml
 kubectl create -f Kubernetes/pg-service.yaml
 kubectl create -f Kubernetes/cli-home.yaml
+kubectl create secret generic grafana_pass --from-literal=grafana_admin_password="$(random_string)"
+kubectl create secret generic postgres_pass --from-literal=pg_postgres_password="$(random_string)"
+kubectl create secret generic gha_admin_pass --from-literal=pg_gha_admin_password="$(random_string)"
