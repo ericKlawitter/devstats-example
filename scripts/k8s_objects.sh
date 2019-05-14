963b77555dd51be9c4b8bfa858dd9bfb83f45ec8
Eric Klawitter <5823687+ericKlawitter@users.noreply.github.com>
2019-05-14 12:12:01 -0700

    fix typo

--- scripts/k8s_objects.sh
+++ scripts/k8s_objects.sh
@@ -9,4 +9,4 @@ kubectl create -f Kubernetes/pg-service.yaml
 kubectl create -f Kubernetes/cli-home.yaml
 kubectl create secret generic grafana-pass --from-literal=grafana_admin_password="$(random_string)"
 kubectl create secret generic postgres-pass --from-literal=pg_postgres_password="$(random_string)"
-kubectl create secret generic gha_admin-pass --from-literal=pg_gha_admin_password="$(random_string)"
+kubectl create secret generic gha-admin-pass --from-literal=pg_gha_admin_password="$(random_string)"
